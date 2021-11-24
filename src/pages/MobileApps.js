dd71765f6e939597a38d080ab426ce0f589955e2
Sepanta <Sepanta_97@Yahoo.com>
2021-11-24 23:07:07 +0330

    adding second and third block

--- src/pages/MobileApps.js
+++ src/pages/MobileApps.js
@@ -1,7 +1,8 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import { makeStyles, useTheme } from "@material-ui/styles";
 import Lottie from "react-lottie";
-import { Link } from "react-router-dom";
+import React from "react";
+
 import {
   Grid,
   Hidden,
@@ -11,6 +12,11 @@ import {
 } from "@material-ui/core";
 import ForwardArrow from "../assets/forwardArrow.svg";
 import BackArrow from "../assets/backArrow.svg";
+import integrationAnimation from "../animations/integrationAnimation/data.json";
+import swiss from "../assets/swissKnife.svg";
+import access from "../assets/extendAccess.svg";
+import engagment from "../assets/increaseEngagement.svg";
+
 const useStyles = makeStyles((theme) => ({
   heading: {
     maxWidth: "40rem",
@@ -33,9 +39,18 @@ const MobileApps = ({ setValue, setSelectedIndex }) => {
   const theme = useTheme();
   const matchesMd = useMediaQuery(theme.breakpoints.down("md"));
   const matchesXs = useMediaQuery(theme.breakpoints.down("xs"));
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: integrationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
     <Grid container direction="column">
+      {/*--------iOS/Android App Block (first)--------*/}
       <Grid
         item
         container
@@ -43,7 +58,6 @@ const MobileApps = ({ setValue, setSelectedIndex }) => {
         className={classes.rowContainer}
         style={{ marginTop: matchesXs ? "1rem" : "2rem" }}
       >
-        {/*--------First Block--------*/}
         <Hidden mdDown>
           <Grid
             item
@@ -76,7 +90,7 @@ const MobileApps = ({ setValue, setSelectedIndex }) => {
               variant="body1"
               paragraph
             >
-             Mobile apps allow you to take your tools on the go.
+              Mobile apps allow you to take your tools on the go.
             </Typography>
             <Typography
               align={matchesMd ? "center" : "inherit"}
@@ -113,6 +127,80 @@ const MobileApps = ({ setValue, setSelectedIndex }) => {
             </IconButton>
           </Grid>
         </Hidden>
+      </Grid>
+      {/*--------Integration and Simultaneous Platform Block(second)--------*/}
+      <Grid item container className={classes.rowContainer}>
+        <Grid item container direction="column" md>
+          <Grid item>
+            <Typography variant="h4" gutterBottom>
+              Integration
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" paragraph>
+              Our technology enables an innate interconnection between web and
+              mobile applications, putting everything you need right in one
+              convenient place.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              This allows you to extend your reach, reinvent interactions, and
+              develop a stronger relationship with your users than ever before.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item md>
+          <Lottie options={defaultOptions} style={{ maxWidth: "20rem" }} />
+        </Grid>
+        <Grid item container direction="column" md>
+          <Grid item>
+            <Typography align="right" variant="h4" gutterBottom>
+              Simultaneous Platform Support
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography align="right" variant="body1" paragraph>
+              Our cutting-edge development process allows us to create apps for
+              iPhone, Android, and tablets — all at the same time.
+            </Typography>
+            <Typography align="right" variant="body1" paragraph>
+              This significantly reduces costs and creates a more unified brand
+              experience across all devices.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      {/*--------Icons Block(third)--------*/}
+      <Grid item container className={classes.rowContainer}>
+        <Grid item container direction="column" alignItems='center'  md>
+          <Grid item>
+            <Typography align='center' variant="h4" gutterBottom>
+              Extend Functionality
+            </Typography>
+          </Grid>
+          <Grid item>
+            <img src={swiss} alt="knife" />
+          </Grid>
+        </Grid>
+        <Grid item container direction="column" alignItems='center' md>
+          <Grid item>
+            <Typography align='center' variant="h4" gutterBottom>
+              Extend Access
+            </Typography>
+          </Grid>
+          <Grid item>
+            <img src={access} alt="tear-one-off sign" style={{maxWidth:'28rem'}} />
+          </Grid>
+        </Grid>
+        <Grid item container direction="column" alignItems='center' md>
+          <Grid item>
+            <Typography align='center' variant="h4" gutterBottom>
+              Increase Engagement
+            </Typography>
+          </Grid>
+          <Grid item>
+            <img src={engagment} alt="app with notification" />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
