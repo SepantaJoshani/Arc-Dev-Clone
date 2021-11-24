b8a25564cd7c84aa92f32131b6a1edeeee047511
Sepanta <Sepanta_97@Yahoo.com>
2021-11-24 12:05:55 +0330

    creating the 4th & 5th block

--- src/pages/CustomSoftware.js
+++ src/pages/CustomSoftware.js
@@ -10,6 +10,9 @@ import stopwatch from "../assets/stopwatch.svg";
 import documentsAnimation from "../animations/documentsAnimation/data";
 import Lottie from "react-lottie";
 import scaleAnimation from "../animations/scaleAnimation/data.json";
+import roots from "../assets/root.svg";
+import automationAnimation from "../animations/automationAnimation/data.json";
+import uxAnimation from "../animations/uxAnimation/data";
 
 const useStyles = makeStyles((theme) => ({
   heading: {
@@ -41,6 +44,22 @@ const CustomSoftware = ({ setSelectedIndex, setValue }) => {
     loop: true,
     autoplay: true,
     animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const automationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: automationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const uxOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: uxAnimation,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
@@ -166,7 +185,7 @@ const CustomSoftware = ({ setSelectedIndex, setValue }) => {
           </Grid>
         </Grid>
       </Grid>
-      <Grid item container>
+      <Grid item container justifyContent="space-around">
         {/*--------Third Block--------*/}
         <Grid item container className={classes.itemContainer} md>
           <Grid item container direction="column" md>
@@ -192,7 +211,7 @@ const CustomSoftware = ({ setSelectedIndex, setValue }) => {
           <Grid item md>
             <Lottie
               options={documentsOptions}
-              style={{ maxHeight: 325, maxWidth: 320, minHeight: 275 }}
+              style={{ maxHeight: 275, maxWidth: 275, minHeight: 250 }}
             />
           </Grid>
         </Grid>
@@ -214,6 +233,85 @@ const CustomSoftware = ({ setSelectedIndex, setValue }) => {
                 Whether you’re a large brand, just getting started, or taking
                 off right now, our application architecture ensures pain-free
                 growth and reliability.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container>
+        {/*--------4th Block--------*/}
+        <Grid item container direction="column" alignItems="center">
+          <Grid item>
+            <img src={roots} alt="tree" height="450rem" width="450rem" />
+          </Grid>
+          <Grid item className={classes.itemContainer}>
+            <Typography align="center" variant="h4" gutterBottom>
+              Root-Cause Analysis
+            </Typography>
+            <Typography align="center" variant="body1" paragraph>
+              Many problems are merely symptoms of larger, underlying issues.
+            </Typography>
+            <Typography align="center" variant="body1" paragraph>
+              We can help you thoroughly examine all areas of your business to
+              develop a holistic plan for the most effective implementation of
+              technology.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container justifyContent="space-around">
+        {/*--------5th Block--------*/}
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4">Automation</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                Why waste time when you don’t have to?
+              </Typography>
+              <Typography variant="body1" paragraph>
+                We can help you identify processes with time or event based
+                actions which can now easily be automated.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Increasing efficiency increases profits, leaving you more time
+                to focus on your business, not busywork.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={automationOptions}
+              style={{ maxHeight: 290, maxWidth: 280 }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item md>
+            <Lottie
+              options={uxOptions}
+              style={{ maxHeight: 310, maxWidth: 155 }}
+            />
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4" align="right">
+                User Experience Design
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="right" paragraph>
+                A good design that isn’t usable isn’t a good design.
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                So why are so many pieces of software complicated, confusing,
+                and frustrating?
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                By prioritizing users and the real ways they interact with
+                technology we’re able to develop unique, personable experiences
+                that solve problems rather than create new ones.
               </Typography>
             </Grid>
           </Grid>
