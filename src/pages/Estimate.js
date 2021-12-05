33d3a662a254531f291b52d675381f6bd77a3886
Sepanta <Sepanta_97@Yahoo.com>
2021-12-05 23:23:22 +0330

    adding the selecting logic and making the estimate component a little space

--- src/pages/Estimate.js
+++ src/pages/Estimate.js
@@ -1,39 +1,23 @@
 import React, { Fragment, useState } from "react";
 import {
   Button,
+  Dialog,
+  DialogContent,
   Grid,
   IconButton,
   Typography,
   useMediaQuery,
+  TextField,
 } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/styles";
 import Lottie from "react-lottie";
-
-import check from "../assets/check.svg";
-import send from "../assets/send.svg";
-import software from "../assets/software.svg";
-import mobile from "../assets/mobile.svg";
-import website from "../assets/website.svg";
 import backArrow from "../assets/backArrow.svg";
 import forwardArrow from "../assets/forwardArrow.svg";
 import backArrowDisabled from "../assets/backArrowDisabled.svg";
 import forwardArrowDisabled from "../assets/forwardArrowDisabled.svg";
-import camera from "../assets/camera.svg";
-import upload from "../assets/upload.svg";
-import person from "../assets/person.svg";
-import persons from "../assets/persons.svg";
-import people from "../assets/people.svg";
-import info from "../assets/info.svg";
-import bell from "../assets/bell.svg";
-import users from "../assets/users.svg";
-import iphone from "../assets/iphone.svg";
-import gps from "../assets/gps.svg";
-import customized from "../assets/customized.svg";
-import data from "../assets/data.svg";
-import android from "../assets/android.svg";
-import globe from "../assets/globe.svg";
-import biometrics from "../assets/biometrics.svg";
-
+import { defaultQuestions } from "../data/data";
+import { websiteQuestions } from "../data/data";
+import { softwareQuestions } from "../data/data";
 import estimateAnimation from "../animations/estimateAnimation/data.json";
 import cloneDeep from "lodash.clonedeep";
 
@@ -54,333 +38,29 @@ const useStyles = makeStyles((theme) => ({
       background: theme.palette.secondary.light,
     },
   },
+  message: {
+    border: `2px solid ${theme.palette.common.blue}`,
+    marginTop: "5em",
+    borderRadius: 5,
+  },
 }));
 
-const defaultQuestions = [
-  {
-    id: 1,
-    title: "Which service are you interested in?",
-    active: true,
-    options: [
-      {
-        id: 1,
-        title: "Custom Software Development",
-        subtitle: null,
-        icon: software,
-        iconAlt: "three floating screens",
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 2,
-        title: "iOS/Android App Development",
-        subtitle: null,
-        icon: mobile,
-        iconAlt: "outlines of phones and tablets",
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 3,
-        title: "Website Development",
-        subtitle: null,
-        icon: website,
-        iconAlt: "computer outline",
-        selected: false,
-        cost: 0,
-      },
-    ],
-  },
-];
 
-const softwareQuestions = [
-  {
-    id: 1,
-    title: "Which service are you interested in?",
-    active: false,
-    options: [
-      {
-        id: 1,
-        title: "Custom Software Development",
-        subtitle: null,
-        icon: software,
-        iconAlt: "three floating screens",
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 2,
-        title: "iOS/Android App Development",
-        subtitle: null,
-        icon: mobile,
-        iconAlt: "outlines of phones and tablets",
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 3,
-        title: "Website Development",
-        subtitle: null,
-        icon: website,
-        iconAlt: "computer outline",
-        selected: false,
-        cost: 0,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Which platforms do you need supported?",
-    subtitle: "Select all that apply.",
-    options: [
-      {
-        id: 1,
-        title: "Web Application",
-        subtitle: null,
-        icon: website,
-        iconAlt: "computer outline",
-        selected: false,
-        cost: 1000,
-      },
-      {
-        id: 2,
-        title: "iOS Application",
-        subtitle: null,
-        icon: iphone,
-        iconAlt: "outline of iphone",
-        selected: false,
-        cost: 1000,
-      },
-      {
-        id: 3,
-        title: "Android Application",
-        subtitle: null,
-        icon: android,
-        iconAlt: "outlines of android phone",
-        selected: false,
-        cost: 1000,
-      },
-    ],
-    active: true,
-  },
-  {
-    id: 3,
-    title: "Which features do you expect to use?",
-    subtitle: "Select all that apply.",
-    options: [
-      {
-        id: 1,
-        title: "Photo/Video",
-        subtitle: null,
-        icon: camera,
-        iconAlt: "camera outline",
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 2,
-        title: "GPS",
-        subtitle: null,
-        icon: gps,
-        iconAlt: "gps pin",
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 3,
-        title: "File Transfer",
-        subtitle: null,
-        icon: upload,
-        iconAlt: "outline of cloud with arrow pointing up",
-        selected: false,
-        cost: 250,
-      },
-    ],
-    active: false,
-  },
-  {
-    id: 4,
-    title: "Which features do you expect to use?",
-    subtitle: "Select all that apply.",
-    options: [
-      {
-        id: 1,
-        title: "Users/Authentication",
-        subtitle: null,
-        icon: users,
-        iconAlt: "outline of a person with a plus sign",
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 2,
-        title: "Biometrics",
-        subtitle: null,
-        icon: biometrics,
-        iconAlt: "fingerprint",
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 3,
-        title: "Push Notifications",
-        subtitle: null,
-        icon: bell,
-        iconAlt: "outline of a bell",
-        selected: false,
-        cost: 250,
-      },
-    ],
-    active: false,
-  },
-  {
-    id: 5,
-    title: "What type of custom features do you expect to need?",
-    subtitle: "Select one.",
-    options: [
-      {
-        id: 1,
-        title: "Low Complexity",
-        subtitle: "(Informational)",
-        icon: info,
-        iconAlt: "'i' inside a circle",
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 2,
-        title: "Medium Complexity",
-        subtitle: "(Interactive, Customizable, Realtime)",
-        icon: customized,
-        iconAlt: "two toggle switches",
-        selected: false,
-        cost: 500,
-      },
-      {
-        id: 3,
-        title: "High Complexity",
-        subtitle: "(Data Modeling and Computation)",
-        icon: data,
-        iconAlt: "outline of line graph",
-        selected: false,
-        cost: 1000,
-      },
-    ],
-    active: false,
-  },
-  {
-    id: 6,
-    title: "How many users do you expect?",
-    subtitle: "Select one.",
-    options: [
-      {
-        id: 1,
-        title: "0-10",
-        subtitle: null,
-        icon: person,
-        iconAlt: "person outline",
-        selected: false,
-        cost: 1,
-      },
-      {
-        id: 2,
-        title: "10-100",
-        subtitle: null,
-        icon: persons,
-        iconAlt: "outline of two people",
-        selected: false,
-        cost: 1.125,
-      },
-      {
-        id: 3,
-        title: "100+",
-        subtitle: null,
-        icon: people,
-        iconAlt: "outline of three people",
-        selected: false,
-        cost: 1.25,
-      },
-    ],
-    active: false,
-  },
-];
-
-const websiteQuestions = [
-  {
-    id: 1,
-    title: "Which service are you interested in?",
-    active: false,
-    options: [
-      {
-        id: 1,
-        title: "Custom Software Development",
-        subtitle: null,
-        icon: software,
-        iconAlt: "three floating screens",
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 2,
-        title: "iOS/Android App Development",
-        subtitle: null,
-        icon: mobile,
-        iconAlt: "outlines of phones and tablets",
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 3,
-        title: "Website Development",
-        subtitle: null,
-        icon: website,
-        iconAlt: "computer outline",
-        selected: false,
-        cost: 0,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Which type of website are you wanting?",
-    subtitle: "Select one.",
-    options: [
-      {
-        id: 1,
-        title: "Basic",
-        subtitle: "(Informational)",
-        icon: info,
-        iconAlt: "person outline",
-        selected: false,
-        cost: 1000,
-      },
-      {
-        id: 2,
-        title: "Interactive",
-        subtitle: "(Users, API's, Messaging)",
-        icon: customized,
-        iconAlt: "outline of two people",
-        selected: false,
-        cost: 2000,
-      },
-      {
-        id: 3,
-        title: "E-Commerce",
-        subtitle: "(Sales)",
-        icon: globe,
-        iconAlt: "outline of three people",
-        selected: false,
-        cost: 2500,
-      },
-    ],
-    active: true,
-  },
-];
 
 const Estimate = () => {
   const classes = useStyles();
   const theme = useTheme();
-  const [questions, setQuestions] = useState(softwareQuestions);
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+
+  const [questions, setQuestions] = useState(defaultQuestions);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [name, setName] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [emailHelper, setEmailHelper] = useState("");
+  const [phone, setPhone] = useState("");
+  const [phoneHelper, setPhoneHelper] = useState("");
+  const [message, setMessage] = useState("");
 
   const defaultOptions = {
     loop: true,
@@ -390,8 +70,6 @@ const Estimate = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
-
-  
 
   const nextQuestion = () => {
     const newQuestions = cloneDeep(questions);
@@ -441,6 +119,83 @@ const Estimate = () => {
     }
   };
 
+  const handleSelect = (id) => {
+    const newQuestions = cloneDeep(questions);
+    const currentlyActive = newQuestions.filter((question) => question.active);
+    const activeIndex = currentlyActive[0].id - 1;
+
+    const newSelected = newQuestions[activeIndex].options[id - 1];
+    const previousSelected = currentlyActive[0].options.filter(
+      (option) => option.selected
+    );
+
+    switch (currentlyActive[0].subtitle) {
+      case "Select one.":
+        if (previousSelected[0]) {
+          previousSelected[0].selected = !previousSelected[0].selected;
+        }
+        newSelected.selected = !newSelected.selected;
+        break;
+      default:
+        newSelected.selected = !newSelected.selected;
+        break;
+    }
+
+    switch (newSelected.title) {
+      case "Custom Software Development":
+        setQuestions(softwareQuestions);
+
+        break;
+
+      case "iOS/Android App Development":
+        setQuestions(softwareQuestions);
+        break;
+
+      case "Website Development":
+        setQuestions(websiteQuestions);
+        break;
+
+      default:
+        setQuestions(newQuestions);
+    }
+  };
+
+  const onChange = (event) => {
+    let isValid;
+
+    switch (event.target.id) {
+      case "email":
+        setEmail(event.target.value);
+        isValid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+          event.target.value
+        );
+
+        if (!isValid) {
+          setEmailHelper("Invalid email");
+        } else {
+          setEmailHelper("");
+        }
+
+        break;
+      case "phone":
+        setPhone(event.target.value);
+        isValid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(
+          event.target.value
+        );
+
+        if (!isValid) {
+          setPhoneHelper("Invalid number");
+        } else {
+          setPhoneHelper("");
+        }
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <Grid container direction="row">
       {/*--------First Block (Left Side)--------*/}
@@ -481,6 +236,7 @@ const Estimate = () => {
                     fontWeight: 500,
                     marginTop: "5em",
                     fontSize: "2.25em",
+                    lineHeight: 1.25,
                   }}
                 >
                   {question.title}
@@ -499,8 +255,24 @@ const Estimate = () => {
 
               <Grid item container>
                 {question.options.map((option) => (
-                  <Grid item container direction="column" md key={option.id}>
-                    <Grid item style={{ maxWidth: "12em" }}>
+                  <Grid
+                    item
+                    container
+                    direction="column"
+                    md
+                    onClick={() => handleSelect(option.id)}
+                    component={Button}
+                    style={{
+                      display: "grid",
+                      textTransform: "none",
+                      borderRadius: 0,
+                      background: option.selected
+                        ? theme.palette.common.orange
+                        : null,
+                    }}
+                    key={option.id}
+                  >
+                    <Grid item style={{ maxWidth: "14em" }}>
                       <Typography
                         variant="h6"
                         align="center"
@@ -556,11 +328,86 @@ const Estimate = () => {
         </Grid>
         {/*--------Second Block (The Button)--------*/}
         <Grid item>
-          <Button variant="contained" className={classes.estimateButton}>
+          <Button
+            onClick={() => setIsDialogOpen(true)}
+            variant="contained"
+            className={classes.estimateButton}
+          >
             Get Estimate
           </Button>
         </Grid>
       </Grid>
+      {/*--------Dialog(Optional Area)--------*/}
+      <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+        <Grid container justifyContent="center">
+          <Typography variant="h2" align="center">
+            Estimate
+          </Typography>
+        </Grid>
+        <DialogContent>
+          <Grid container>
+            {/*--------Dialog 's Left Side --------*/}
+            <Grid item container direction="column">
+              <Grid item style={{ marginBottom: "0.5em" }}>
+                <TextField
+                  fullWidth
+                  label="Name"
+                  id="name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </Grid>
+
+              <Grid item style={{ marginBottom: "0.5em" }}>
+                <TextField
+                  error={emailHelper.length !== 0}
+                  helperText={emailHelper}
+                  fullWidth
+                  label="Email"
+                  id="email"
+                  value={email}
+                  onChange={onChange}
+                />
+              </Grid>
+
+              <Grid item style={{ marginBottom: "0.5em" }}>
+                <TextField
+                  error={phoneHelper.length !== 0}
+                  helperText={phoneHelper}
+                  fullWidth
+                  label="Phone"
+                  id="phone"
+                  value={phone}
+                  onChange={onChange}
+                />
+              </Grid>
+
+              <Grid item style={{ minWidth: matchesXS ? "20em" : "30em" }}>
+                <TextField
+                  fullWidth
+                  InputProps={{ disableUnderline: true }}
+                  value={message}
+                  className={classes.message}
+                  id="message"
+                  onChange={(event) => setMessage(event.target.value)}
+                  multiline
+                  minRows={10}
+                />
+              </Grid>
+              <Grid item>
+                <Typography variant="body1" paragraph>
+                  We can create this digital solution for an estimated
+                </Typography>
+                <Typography variant="body1" paragraph>
+                  Fill out your name, number, and email, place your request, and
+                  we’ll get back to you with details moving forward and a final
+                  price.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
     </Grid>
   );
 };
